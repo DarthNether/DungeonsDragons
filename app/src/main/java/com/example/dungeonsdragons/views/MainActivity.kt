626144e507e8ae62d8ece80483b848ddd9f3b17d
DarthNether <jvfronton@gmail.com>
2019-09-23 22:41:05 +0200

    Initial commit. Gradle dependencies properly managed. Application scheme created

---
+++ app/src/main/java/com/example/dungeonsdragons/views/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.dungeonsdragons.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dungeonsdragons.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
