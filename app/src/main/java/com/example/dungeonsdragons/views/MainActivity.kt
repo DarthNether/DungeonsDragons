e932bae92b1a896a10f63170fbe59e915da5e78d
DarthNether <jvfronton@gmail.com>
2019-11-04 19:34:20 +0100

    First commit with Navigation Drawer Main. Not properly implemented yet. Still default Navigation Drawer Android's implementation.

--- app/src/main/java/com/example/dungeonsdragons/views/MainActivity.kt
+++ app/src/main/java/com/example/dungeonsdragons/views/MainActivity.kt
@@ -1,35 +1,57 @@
 package com.example.dungeonsdragons.views
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.dungeonsdragons.R
-import com.example.dungeonsdragons.dao.AbilityDao
-import com.example.dungeonsdragons.dao.RaceDao
-import com.example.dungeonsdragons.dao.SkillDao
-import com.example.dungeonsdragons.database.DnDDatabase
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var db: DnDDatabase
-    private lateinit var abilityDao: AbilityDao
-    private lateinit var skillDao: SkillDao
-    private lateinit var raceDao: RaceDao
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-//        db = Room.inMemoryDatabaseBuilder(this.applicationContext, DnDDatabase::class.java).build()
-//
-//        raceDao = db.raceDao()
-//
-//        ioThread {
-//            raceDao.insertRaces(*PREPOPULATE_RACES)
-//
-//            val byName = raceDao.loadRaceByName("Tiefling")
-//            Log.w("Bonus: ", byName[0].name)
-//        }
-//
-//        db.close()
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
